b408de7d5c695fbe158f1d013ef0fb78773dcd7a
yanismdz <maoucha.yanis@gmail.com>
2016-09-25 16:36:59 +0200

    presque fini

---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+	<img class="background-img" src="img/1846.jpg">
+	<header>
+	    <div class="logo">Yanis Maoucha</div>
+	    <div class="mobile-menu">
+		+
+		<nav class="nav-m">
+		    <a class="navlink-m-1"  href="#">Acceuil</a>
+		    <a class="navlink-m-2"  href="#">Qui suis je?</a>
+		    <a class="navlink-m-3"  href="#">Mes Competence</a>
+		    <a class="navlink-m-4"  href="#">Mes Projets</a>
+		</nav>
+	    </div>
+	    <nav class="nav-d">
+		<a class="navlink"  href="#">Acceuil</a>
+		<a class="navlink"  href="#">Qui suis je?</a>
+		<a class="navlink"  href="#">Mes Projets</a>
+		<a class="navlink"  href="#">Mes Competence</a>
+	    </nav>
+	</header>
+	<section class="jumbo">
+	    <h1 class="jumbo-style">Yanis Maoucha Portfolio</h1>
+	</section>
+	<section class="qui">
+	    
+	    <h2>Qui suis je?</h2>
+	    <hr>
+	    <article class="description">
+		<img class="img-d" src="img/05.jpg" alt="goku">
+		<p>Bienvenu sur mon Portfolio, moi c'est Yanis Maoucha développeur web front et backend, a la comprehension rapide comme l'éclaire jaune de konoha. Passioné d'informatique depuis le plus jeune age pére informaticien oblige, le developpement web etais le choix évident depuis la primaire. A la fin de mes étude scolaire j'ai intégré l'école épitech pour une année puis la formation simplon a simplon roubaix.
+		    
+		</p>
+	    </article>
+	</section>
+	<section class="projet">
+	    <img src="img/bg2.jpg" class="bgimg2">
+	    <h2 class="jumbo-style h2p">Mes Projets</h2>
+	    <div class="list-projet jumbo-style">
+		<div class="projet-c">
+		    <img class="img-projet" src="img/mmh.png">
+		    <div class="dc">
+			<h3>My Money Help</h3>
+			<p>Plateforme de crowdfounding social</p>
+		    </div>
+		</div>
+	    </div>
+	</section>
+	<section class="interupt"></section>
+	<section class="break"></section>
+	<section class="skills"></sectrion>
+            <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+            <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+
+		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+		<script>
+		    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+		    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		    e.src='https://www.google-analytics.com/analytics.js';
+		    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+		    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+		</script>
+    </body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(".mobile-menu").click(function(){
+    var $S = 1;
+    var $navlink = ".navlink-m-" + $S;
+    function addshow(){
+	alert("ntm");
+
+	alert($navlink);
+    }
+    for ($S = 1; $S <= 4; $S++){
+
+	$navlink = "navlink-m-" + $S;
+	var w = setInterval($($navlink).addClass("show"), 1000);
+	alert($navlink);
+    }
+
+});
+
+$(window).scroll(function(){
+    var qui = $(".qui").position();// distance a parcourir pour que la bar change de coule\
+    var projet = $(".projet").position();
+    var quiimg = $(".img-d").position();
+    var scrolledFromtop = $(window).scrollTop();
+    var imgsrc = $(".background-img").attr("src");
+    if(scrolledFromtop > (quiimg.top - 400))
+    {
+	$(".description img").addClass("fadein");
+	$(".description p").addClass("fadein");
+    }
+    if(scrolledFromtop > projet.top)
+    {
+	$(".background-img").attr("src", "img/bg3.jpg");
+    }
+    else{
+	$(".background-img").attr("src", "img/1846.jpg");
+    }
+    if(scrolledFromtop > (qui.top -45) && scrolledFromtop < (projet.top - 45))
+    {
+	$(".logo").addClass("whitened");
+	$(".navlink").addClass("whitened");
+    }
+    else{	
+	$(".navlink").removeClass("whitened");
+	$(".logo").removeClass("whitened");
+    }
+
+});
